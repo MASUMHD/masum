109d58aa28dadf8294585fa96810531d99e15fbe
masumhd <mrmasum2k4@gmail.com>
2026-08-03 10:27:14 +0600

    add project section card design add

--- masum/app/(homeSections)/projects/ProjectCard.tsx
+++ masum/app/(homeSections)/projects/ProjectCard.tsx
@@ -1,30 +1,32 @@
-import { FiExternalLink, FiGithub } from "react-icons/fi";
+import Image from "next/image";
+import { FiArrowRight, FiExternalLink, FiGithub } from "react-icons/fi";
+import workImage from "@/public/work_image.jpeg";
+import Link from "next/link";
 
 export default function ProjectCard({ item }: any) {
   return (
-    <div className="rounded-2xl overflow-hidden border bg-white shadow-sm hover:shadow-md transition group">
-      
-      {/* TOP GRADIENT */}
-      <div className="h-40 bg-linear-to-br from-orange-300 to-pink-400 relative">
-        
-        {/* CATEGORY BADGE */}
-        <span className="absolute top-3 left-3 text-xs px-3 py-1 rounded-full bg-white/80 backdrop-blur">
-          {item.category}
-        </span>
+    <div className="bg-white border border-gray-200 rounded-lg hover:shadow-lg hover:border-orange-400 hover:-translate-y-1 transition duration-300 ease-in-out">
+      {/* PROJECT IMAGE */}
+      <div className="h-40 ">
+        <Image
+          height={160}
+          width={400}
+          src={workImage}
+          alt="Project Image"
+          className="w-full h-full object-cover rounded-t-lg"
+        />
       </div>
 
       {/* CONTENT */}
       <div className="p-5">
-        <h3 className="font-semibold text-lg mb-2">
-          {item.title}
-        </h3>
+        <h3 className="font-semibold text-lg mb-2">{item.title}</h3>
 
         {/* TECH STACK */}
-        <div className="flex flex-wrap gap-2 mb-4">
+        <div className="flex flex-wrap gap-2 mb-6">
           {item.tech.map((t: string, i: number) => (
             <span
               key={i}
-              className="text-xs px-2 py-1 bg-gray-100 rounded-full"
+              className="text-xs px-4 py-1 rounded-full bg-gray-100 border border-orange-500 font-semibold text-gray-800"
             >
               {t}
             </span>
@@ -32,13 +34,30 @@ export default function ProjectCard({ item }: any) {
         </div>
 
         {/* LINKS */}
-        <div className="flex gap-4 text-sm text-gray-600">
-          <a href="#" className="flex items-center gap-1 hover:text-black">
-            Live <FiExternalLink size={14} />
-          </a>
-          <a href="#" className="flex items-center gap-1 hover:text-black">
-            Code <FiGithub size={14} />
-          </a>
+        <div className="flex gap-4 justify-between text-sm text-gray-600 pt-3 border-t border-dotted border-orange-400">
+          <div className="flex gap-4">
+            <Link
+              href="#"
+              className="flex items-center gap-1 hover:text-orange-400 hover:font-semibold"
+            >
+              Live <FiExternalLink size={14} />
+            </Link>
+            <Link
+              href="#"
+              className="flex items-center gap-1 hover:text-orange-400 hover:font-semibold"
+            >
+              Code <FiGithub size={14} />
+            </Link>
+          </div>
+
+          <div>
+            <Link
+              href="#"
+              className="flex items-center gap-1 hover:text-orange-400 hover:font-semibold"
+            >
+              View More <FiArrowRight size={14} />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
